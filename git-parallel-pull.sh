afb915a3bed1fe087053a4830984172da4a61574
Bartosz Filejecki <filus1025@gmail.com>
2020-12-21 23:32:07 +0100

    Add git-parallel-pull script

---
+++ git-parallel-pull.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+which parallel > /dev/null 2>&1 || { echo "GNU Parallel not found on PATH, script will exit"; exit 1; }
+which git > /dev/null 2>&1 || { echo "git not found on PATH, script will exit"; exit 1; }
+
+DIRECTORY=${1:?"Missing directory argument"}
+
+find "$DIRECTORY" -type d -name ".git" | parallel git --git-dir={} --work-tree={//} pull
